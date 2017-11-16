9c209275e2ee2f38a052fdc722beb5cf4ddd0114
Rok <Rok>
2017-11-16 02:04:19 +0100

    added image search app

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 var express = require("express"),
     path = require("path"),
     metadata = require("./file_metadata/metadata"),
+    imageSearch = require("./image_search/imageSearch"),
     port = process.env.PORT || 3000,
     app = express();
 
+    process.env.NODE_ENV = "production";
+
     app.use(express.static(path.join(__dirname, "./public/")));
     app.use("/metadata", metadata);
-
+    app.use("/image-search", imageSearch);
 
     app.listen(port, () => console.log("Listening on port " + port))
